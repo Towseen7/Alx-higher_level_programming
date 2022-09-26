9674e9453d4e91b48b37f328b1374e0ae03f8b44
Ajayi Oluwatosin <ajayioluwatosin777@gmail.com>
2022-09-26 15:46:16 -0700

    task4

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-for a in range(ord('a'), ord('z') + 1):
-    if chr(a) != 'e' and chr(a) != 'q':
-        print("{:c}".format(a), end='')
+for num in range(0, 99):
+    print("{:d} = 0x{:x}".format(num, num))
