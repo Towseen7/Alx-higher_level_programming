3a9a4fb7fdf0503a97706a7bc48f920cb3ff8ea4
Ajayi Oluwatosin <ajayioluwatosin777@gmail.com>
2022-09-28 21:55:56 -0700

    task12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,13 +1,20 @@
 #!/usr/bin/python3
-
-
 def roman_to_int(roman_string):
-    if not isinstance(roman_string, str):
+    if type(roman_string) is not str or roman_string is None:
         return 0
-    total = 0
-    num = 0
-    digits = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    for r in reversed(roman_string):
-        num = digits[r]
-        total += num if total < num * 5 else -num
-        return total
+    roman_dict = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+    }
+    decs = [roman_dict[x] for x in roman_string]
+    output = 0
+    for i in range(len(decs)):
+        output += decs[i]
+        if decs[i - 1] < decs[i] and i != 0:
+            output -= (decs[i - 1] + decs[i - 1])
+            return output
