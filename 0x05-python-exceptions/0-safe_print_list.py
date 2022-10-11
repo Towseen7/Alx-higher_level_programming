b3b0129893fa69a670cb59fbfb6aceba772b6813
Ajayi Oluwatosin <ajayioluwatosin777@gmail.com>
2022-10-11 05:17:59 -0700

    task0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
+
+
 def safe_print_list(my_list=[], x=0):
     n = 0
-    for m in range(x):
         try:
-            print(my_list[m], end="")
-            n += 1
+            while n is not x:
+                print(my_list[n], end='')
+                n += 1
         except IndexError:
-            break
+            None
         print()
         return n
